15ed8ee374674fbc783c42bb78dcc143523e56fd
dfrever <dfreverbel@gmail.com>
2021-12-07 18:55:28 -0300

    Mudando nome projeto, teste bobo highcharts

--- desafioBrMed/desafioBrMed/asgi.py
+++ desafioBrMed/desafioBrMed/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for brMed project.
+ASGI config for desafioBrMed project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brMed.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'desafioBrMed.settings')
 
 application = get_asgi_application()
